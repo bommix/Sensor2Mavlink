8f80e40c3258e19b857325938dca1f92ce067228
bommix <m.bommhardt@gmail.com>
2022-11-04 00:26:58 +0000

    Installscript pip3

--- app/ma.py
+++ app/ma.py
@@ -1,17 +1,3 @@
-#! /usr/bin/env python3
-from pathlib import Path
-
-import appdirs
-import uvicorn
-from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, HTTPException, status
-from fastapi.responses import HTMLResponse, FileResponse
-from fastapi_versioning import VersionedFastAPI, version
-from loguru import logger
-from gpiozero import Servo
-
-from pydantic import BaseModel
-
 import asyncio
 import json
 import os
@@ -21,47 +7,10 @@ import sys
 
 import aiohttp
 
+#from commonwealth.mavlink_comm.MavlinkComm import MavlinkMessenger
+#from loguru import logger
 
-class TextData(BaseModel):
-    data: str
 
-
-SERVICE_NAME = "Sensor2Mavlink"
-
-app = FastAPI(
-    title="Sensor2Mavlink",
-    description="API to send Sensors to Mavlink",
-)
-
-logger.info(f"Starting {SERVICE_NAME}!")
-
-servos = {}
-
-@app.post("/setServo", status_code=status.HTTP_200_OK)
-@version(1, 0)
-async def set_servo(pin: int, pwm: float) -> Any:
-    if pin < 0 or pwm < -1 or pwm > 1:
-        raise HTTPException("Invalid values")
-    if pin not in servos:
-        servos[pin] = Servo(pin)
-    servos[pin].value = pwm
-    test=MM()
-    test.send_statustext(str("Hallo"))
-    return "ok"
-
-app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
-
-app.mount("/", StaticFiles(directory="static",html = True), name="static")
-
-@app.get("/", response_class=FileResponse)
-async def root() -> Any:
-        return "index.html"
-
-if __name__ == "__main__":
-    # Running uvicorn with log disabled so loguru can handle it
-    uvicorn.run(app, host="0.0.0.0", port=8120, log_config=None)
-    
-    
 class MM:
     def __init__(self) -> None:
 
@@ -115,3 +64,5 @@ class MM:
             except asyncio.exceptions.TimeoutError as error:
                 print(f"Request timed out after {request_timeout} second.") 
                 await asyncio.sleep(1)
+test=MM()
+test.send_statustext(str(sys.argv[1]))
