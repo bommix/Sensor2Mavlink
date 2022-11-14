5adc4ada32249e7caf695d89a8948164520c8d7a
bommix <m.bommhardt@gmail.com>
2022-11-14 12:38:59 +0000

    Mon 14 Nov 12:38:59 GMT 2022

--- app/setup.py
+++ app/setup.py
@@ -2,8 +2,18 @@
 
 import os
 import ssl
-
+import appdirs
+from pathlib import Path
 from setuptools import setup
+
+files = ["file.txt", "ph.txt","tds.txt","o2.txt","turbidity.txt","o2_calib.txt","tds_calib.txt","ph_calib.txt","turbidity_calib.txt"]
+user_config_dir = Path(appdirs.user_config_dir())
+fruits = ["apple", "banana", "cherry"]
+for x in files:
+  text_file=user_config_dir /  x
+  f=open(text_file, "w")
+  f.write(str(1.0))
+
 
 # Ignore ssl if it fails
 if not os.environ.get("PYTHONHTTPSVERIFY", "") and getattr(ssl, "_create_unverified_context", None):
@@ -25,3 +35,5 @@ setup(
         "gpiozero==1.6.2",
     ],
 )
+
+
